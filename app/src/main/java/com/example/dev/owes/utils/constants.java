80715ac5a9acea89aea835a057571c65d0c4037f
dev <dev@MacBook-Air-de-dev.local>
2017-01-03 13:28:18 -0600

    connect firebase to your app

---
+++ app/src/main/java/com/example/dev/owes/utils/constants.java
@@ -0,0 +1,14 @@
+package com.example.dev.owes.utils;
+
+import com.example.dev.owes.BuildConfig;
+
+/**
+ * Created by dev on 03/01/17.
+ */
+
+public final class constants {
+    /**
+     * Constants for Firebase URL
+     */
+    public static final String FIREBASE_URL = BuildConfig.UNIQUE_FIREBASE_ROOT_URL;
+}
